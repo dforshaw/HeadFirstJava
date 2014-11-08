6c09d8f40727eae0ef00625b573c4ce7a48c6245
David Forshaw <dfcv126@gmail.com>
2014-11-08 15:34:41 -0800

    more code checkin

---
+++ src/main/java/com/headfirstjava/ch3/Triangle.java
@@ -0,0 +1,26 @@
+package com.headfirstjava.ch3;
+
+public class Triangle {
+
+    double area;
+    int height;
+    int length;
+
+    public static void main(String[] args) {
+
+        while (   ) {
+
+        }
+
+        x = 27;
+        Triangle t5 = ta[2];
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", t5 area = " + t5.area);
+    }
+
+    void setArea() {
+
+        area = (height * length) / 2;
+    }
+}
