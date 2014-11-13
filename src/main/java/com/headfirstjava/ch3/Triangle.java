39304e4e08a5f813d4f55460148ee1fbd4c00ccf
David Forshaw <dfcv126@gmail.com>
2014-11-12 17:29:35 -0800

    more code checkin

--- src/main/java/com/headfirstjava/ch3/Triangle.java
+++ src/main/java/com/headfirstjava/ch3/Triangle.java
@@ -7,11 +7,22 @@ public class Triangle {
     int length;
 
     public static void main(String[] args) {
+        int x = 0;
+        Triangle [] ta = new Triangle[4];
 
-        while (   ) {
+        while ( x < 4 ) {
+            ta[x] = new Triangle();
 
+            ta[x].height = (x + 1) * 2;
+            ta[x].length = x + 4;
+            ta[x].setArea();
+
+            System.out.print("triangle " + x + ", area");
+            System.out.println(" = " + ta[x].area);
+
+            x = x + 1;
         }
-
+        int y = x;
         x = 27;
         Triangle t5 = ta[2];
         ta[2].area = 343;
